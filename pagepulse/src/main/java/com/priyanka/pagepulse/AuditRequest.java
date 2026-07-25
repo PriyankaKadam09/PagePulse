86304767fa3965437b9cd399d118443041c9476e
Priyanka Kadam <piu972004@gmail.com>
2026-07-25 08:02:37 +0530

    Initial commit - PagePulse URL Checker.

--- pagepulse/src/main/java/com/priyanka/pagepulse/AuditRequest.java
+++ pagepulse/src/main/java/com/priyanka/pagepulse/AuditRequest.java
@@ -1,10 +1,15 @@
 package com.priyanka.pagepulse;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class AuditRequest {
 
     @NotBlank(message = "URL is required")
+    @Pattern(
+            regexp = "^(http|https)://.*$",
+            message = "Please enter a valid URL starting with http:// or https://"
+    )
     private String url;
 
     public String getUrl() {
